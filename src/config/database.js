3391e14d600250528935a12330cf41921043c29d
Rafael Trigo <rafael.trigo.azevedo@gmail.com>
2021-03-19 15:08:00 -0300

    db config

--- src/config/database.js
+++ src/config/database.js
@@ -24,6 +24,7 @@ module.exports = {
   build: {
     dialect: 'postgres',
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
