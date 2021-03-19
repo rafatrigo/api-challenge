39180abaca3b33af7d6ec159578f08b874a18016
Rafael Trigo <rafael.trigo.azevedo@gmail.com>
2021-03-19 13:26:39 -0300

    updated

---
+++ src/config/database.js
@@ -0,0 +1,34 @@
+require('dotenv/config.js');
+
+module.exports = {
+  dev: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'api-challenge',
+    define: {
+      timestamps: true,
+    },
+  },
+  test: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'api-challenge-test',
+    define: {
+      timestamps: true,
+    },
+  },
+  build: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    define: {
+      timestamps: true,
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,38 +1,37 @@
-import 'dotenv/config.js'
-import 'express-async-errors'
-import './database/index.js'
-import 'babel-polyfill'
+import 'dotenv/config.js';
+import 'express-async-errors';
+import './database/index.js';
+import 'babel-polyfill';
 
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import AppError from './Error/AppError.js'
-import routes from './http/routes/index.js'
+import AppError from './Error/AppError.js';
+import routes from './http/routes/index.js';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.use(
-  (err, requerst, response, next) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
+app.use((err, requerst, response, next) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
-  },
-);
+  }
 
-app.listen(3333, () => {
-  console.log('🚀 server started on port 3333')
-})
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log('🚀 server started on port ', port);
+});
